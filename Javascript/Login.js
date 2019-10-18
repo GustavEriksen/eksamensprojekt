39468387def29d540714debf9a1d39699ae23149
SebastianRosenquist <seb_rosenquist@hotmail.com>
2019-10-18 13:34:29 +0200

    forsat arbejde med login og bruger

---
+++ Javascript/Login.js
@@ -0,0 +1,11 @@
+// Create submit for sign-up and log-in.
+var submit = document.getElementById('submit');
+var login = document.getElementById('login');
+var existingUser = JSON.parse(localStorage.getItem('User'));
+
+//When clicking the Login button, retrieve input of username and password input-boxes
+login.onclick = function () {
+
+    var usernameInput = document.getElementById("")
+
+}
