ce2ed2a57c83ce95e45e0d1455d4e32ae71f63df
SebastianRosenquist <seb_rosenquist@hotmail.com>
2019-10-18 12:44:48 +0200

    Forsat arbejde med login og register

---
+++ Javascript/userList.js
@@ -0,0 +1,11 @@
+// This JS is for the user database. It checks to see if any user already exists in localStorage. If now, then it creates and pushes one.
+if(localStorage.getItem('user') == null) {
+    var userList = [];
+        userList.push(new User ("admin", "admin.vivian@vivian.dk", +2561659662, 1994, Admin));
+
+        //Stringify the userlist and saves in localStorage
+        var userListString = JSON.stringify(userList)
+        localStorage.setItem('User', userListString)
+} else {
+    var userList = JSON.parse(localStorage.getItem('User'))
+}
