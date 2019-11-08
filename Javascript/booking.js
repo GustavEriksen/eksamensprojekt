425d07c11a019f054fe1c45e4413c441cd07ec2a
lucas skytte <lucasskytte@hotmail.com>
2019-11-08 11:31:11 +0100

    booking

--- Javascript/booking.js
+++ Javascript/booking.js
@@ -1,15 +1,4 @@
- function validate() {
-    var name = document.getElementById('name').value;
-
-
-    return alert('Hiya,' + name);
-  }
-
-  function myfunction(){
-    var x = document.getElementById("mydiv");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-  }
+if (JSON.parse(localStorage.getItem("currentUser")) == null){
+    alert("You need to be logged in to make a booking request");
+    document.location.href = "home.html";
+}
