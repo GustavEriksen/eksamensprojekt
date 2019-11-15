45bcc46760ebb1bd0a21957ac9593518537b5e9c
Nina <nifo19ac@student.cbs.dk>
2019-11-15 10:29:17 +0100

    forsøg på at lukke ved klik udenfor popup

--- Javascript/login_navbar.js
+++ Javascript/login_navbar.js
@@ -1,7 +1,21 @@
+// This function retrives the HTML tag 'onclick' to open the login popup in the nav bar
+function openForm() {
+    document.getElementById("loginForm").style.display = "block";
+}
+
+// This function retrives the HTML tag 'onclick' to close the login popup in the nav bar
 function closeForm() {
     document.getElementById("loginForm").style.display = "none";
 }
 
-function openForm() {
-    document.getElementById("loginForm").style.display = "block";
+// This function will make the login popup close when clicking outside of the form
+// Firstly, we define a variable to retrieve the div of the login form
+var closeOutside = document.getElementById('id01')
+
+// Then we use 'event' with the target property to get the variable above and make it disappear
+window.onclick = function(event) {
+    if (event.target == closeOutside) {
+        closeOutside.style.display = "none";
+    }
 }
+// The function above is currently not working
