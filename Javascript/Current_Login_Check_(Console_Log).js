1b5e655ad9d3db31146d3b4ba11928711945af37
SebastianRosenquist <seb_rosenquist@hotmail.com>
2019-11-21 11:18:38 +0100

    Current login printed in consoleLog

---
+++ Javascript/Current_Login_Check_(Console_Log).js
@@ -0,0 +1,8 @@
+var currentLogin
+if(localStorage.getItem("currentUser") == null){
+    console.log("There is currently no user registered as logged-in");
+} else {
+    currentLogin = JSON.parse(localStorage.getItem("currentUser"))
+    console.log("Currently logged-in as:")
+    console.log(currentLogin);
+}
