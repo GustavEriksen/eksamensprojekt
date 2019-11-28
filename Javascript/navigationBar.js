f26b76b81912a173d43c956a51739786a8a4004e
Nina <nifo19ac@student.cbs.dk>
2019-11-28 11:19:44 +0100

    close funktion v klik udenfor hamburgerbar

--- Javascript/navigationBar.js
+++ Javascript/navigationBar.js
@@ -1,9 +1,9 @@
-// This function retrives the HTML tag 'onclick' to open the login popup in the nav bar
+// This function uses the HTML tag 'onclick' to open the login popup in the nav bar
 function openLoginForm() {
     document.getElementById("loginForm").style.display = "block";
 }
 
-// This function retrives the HTML tag 'onclick' to close the login popup in the nav bar
+// This function uses the HTML tag 'onclick' to close the login popup in the nav bar
 function closeLoginForm() {
     document.getElementById("loginForm").style.display = "none";
 }
@@ -21,11 +21,28 @@ function closeLoginForm() {
             // The function above is currently not working
 
 <!-- Script for the hamburger bar -->
-function openHamburger() {
-    var hamburger = document.getElementById("hamburgerPopUp");
-    if (hamburger.className === "hamburgerBar") {
-        hamburger.className += " responsive";
-    } else {
-        hamburger.className = "hamburgerBar";
-    }
-}
+// Script for the hamburger bar
+var hamburger = document.getElementById("hamburgerPopUp");
+var navWrapper = document.getElementById("navBarWrapper");
+
+navWrapper.style.display = 'none'
+navWrapper.style.height = '100vh'
+navWrapper.style.width = '120%'
+
+
+// open menu
+document.querySelector('.icon').addEventListener('click', () => {
+    hamburger.classList.add('responsive')
+    navWrapper.style.display = 'block'
+    navWrapper.style.position = 'absolute'
+    navWrapper.style.top = '0'
+    navWrapper.style.zIndex = '102'
+    navWrapper.style.backgroundColor = 'rgba(0,0,0, .5)'
+    navWrapper.style.marginLeft = '-1%'
+})
+
+// close menu
+document.querySelector('#navBarWrapper').addEventListener('click', () => {
+    hamburger.classList.remove('responsive')
+    navWrapper.style.display = 'none'
+})
