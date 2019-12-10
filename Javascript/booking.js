b64a40a33bec4771adb7dc1ab2a8c6896cd5cdb2
lucas skytte <lucasskytte@hotmail.com>
2019-12-10 11:51:16 +0100

    hey

--- Javascript/booking.js
+++ Javascript/booking.js
@@ -2,11 +2,11 @@
 if (JSON.parse(localStorage.getItem("currentUser")) == null){
     alert("You need to be logged in to make a booking request");
     document.location.href = "home.html";
-    var x = document.getElementById("mydiv");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+    var bookingdiv = document.getElementById("booking-div");
+    if (bookingdiv.style.display === "none") {
+        bookingdiv.style.display = "block";
     } else {
-        x.style.display = "none";
+        bookingdiv.style.display = "none";
     }
 }
 
